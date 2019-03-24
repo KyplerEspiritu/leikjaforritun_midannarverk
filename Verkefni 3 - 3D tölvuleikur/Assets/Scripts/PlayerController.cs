0547d8758df645ba889ad79aad170fa04ea19727
KyplerEspiritu <kypleres312@nemi.tskoli.is>
2019-03-24 23:17:32 +0000

    Update PlayerController.cs

--- Verkefni 3 - 3D tölvuleikur/Assets/Scripts/PlayerController.cs
+++ Verkefni 3 - 3D tölvuleikur/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
 	private int treesCollected;
     // Start is called before the first frame update
 
-    IEnumerator loadMainMenu()
+    IEnumerator loadMainMenu() // Fall sem loadar main menu eftir 3 sek
     {
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene(0);
@@ -31,25 +31,25 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() // Fall fyrir hreyfingu kúlunnar
     {
         var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
  		var actualDirection = Camera.main.transform.TransformDirection(movement);
  		rb.AddForce(actualDirection * speed);
     }
     void OnTriggerEnter(Collider other){
-    	if (other.gameObject.CompareTag("Pick Up")){
+    	if (other.gameObject.CompareTag("Pick Up")){ // Þegar þú snertir object með tagið pick up
     		other.gameObject.SetActive(false);
     		treesCollected++;
     		SetCountTex();
     	}
-    	if (other.gameObject.CompareTag("trophy")){
+    	if (other.gameObject.CompareTag("trophy")){ // Þegar þú nærð í trophy-ið
     		other.gameObject.SetActive(false);
     		winText.text = "You escaped!";
             StartCoroutine(loadMainMenu());
     	}
     }
-    void SetCountTex(){
+    void SetCountTex(){ // Update-ar textan 
     	countText.text = "Keys Collected: " + treesCollected.ToString();
     	if (treesCollected >= 5){
     		GameObject[] gos = GameObject.FindGameObjectsWithTag("purpleBox");
